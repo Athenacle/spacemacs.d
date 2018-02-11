6a53ac448693ef1ba9839dbbb0fc47ae61028ac5
WangXiao <zjjhwxc@gmail.com>
2018-02-11 14:44:54 +0800

    init-others: remove rcirc config and add c-mode config

--- lisp/init-others.el
+++ lisp/init-others.el
@@ -1,36 +1,159 @@
-;;; rcirc start
+;;; init-others.el --- init other settings
+;;
+;; Filename: init-others.el
+;; Description: init other settings that doesnot belongs to spacemacs
+;; Author: WangXiao
+;; Copyright 2018 (c) WangXiao;; Created: Sat Jan 13 15:32:00 2018 (+0800)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;; Some features that have no interact which spacemacs.
+;; 1. C modes style set.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change Log:
+;; 1. Jan 13, 2018
+;;    Init Version. Setup C style
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; C mode style start.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun google-c-lineup-expression-plus-4 (langelem)
+  "Indents to the beginning of the current C expression plus 4 spaces.
+This implements title \"Function Declarations and Definitions\"
+of the Google C++ Style Guide for the case where the previous
+line ends with an open parenthese.
+\"Current C expression\", as per the Google Style Guide and as
+clarified by subsequent discussions, means the whole expression
+regardless of the number of nested parentheses, but excluding
+non-expression material such as \"if(\" and \"for(\" control
+structures.
+Suitable for inclusion in `c-offsets-alist'."
+  (save-excursion
+    (back-to-indentation)
+    ;; Go to beginning of *previous* line:
+    (c-backward-syntactic-ws)
+    (back-to-indentation)
+    (cond
+     ;; We are making a reasonable assumption that if there is a control
+     ;; structure to indent past, it has to be at the beginning of the line.
+     ((looking-at "\\(\\(if\\|for\\|while\\)\\s *(\\)")
+      (goto-char (match-end 1)))
+     ;; For constructor initializer lists, the reference point for line-up is
+     ;; the token after the initial colon.
+     ((looking-at ":\\s *")
+      (goto-char (match-end 0))))
+    (vector (+ 4 (current-column)))))
+
+(defconst athenacle|c-style
+  `((c-recognize-knr-p . nil)
+    (c-basic-offset . 4)
+    (indent-tabs-mode . nil)
+    (c-comment-only-line-offset . 0)
+    (c-hanging-braces-alist . ((defun-open after)
+                               (defun-close before after)
+                               (class-open after)
+                               (class-close before after)
+                               (inexpr-class-open after)
+                               (inexpr-class-close before)
+                               (namespace-open after)
+                               (inline-open after)
+                               (inline-close before after)
+                               (block-open after)
+                               (block-close . c-snug-do-while)
+                               (extern-lang-open after)
+                               (extern-lang-close after)
+                               (statement-case-open after)
+                               (substatement-open after)))
+    (c-hanging-colons-alist . ((case-label)
+                               (label after)
+                               (access-label after)
+                               (member-init-intro before)
+                               (inher-intro)))
+    (c-hanging-semi&comma-criteria
+     . (c-semi&comma-no-newlines-for-oneline-inliners
+        c-semi&comma-inside-parenlist
+        c-semi&comma-no-newlines-before-nonblanks))
+    (c-indent-comments-syntactically-p . t)
+    (comment-column . 80)
+    (c-indent-comment-alist . ((other . (space . 2))))
+    (c-cleanup-list . (brace-else-brace
+                       brace-elseif-brace
+                       brace-catch-brace
+                       empty-defun-braces
+                       defun-close-semi
+                       list-close-comma
+                       scope-operator))
+    (c-offsets-alist . ((arglist-intro google-c-lineup-expression-plus-4)
+                        (func-decl-cont . ++)
+                        (member-init-intro . ++)
+                        (inher-intro . ++)
+                        (comment-intro . 0)
+                        (arglist-close . c-lineup-arglist)
+                        (topmost-intro . 0)
+                        (block-open . 0)
+                        (inline-open . 0)
+                        (substatement-open . 0)
+                        (statement-cont
+                         .
+                         (,(when (fboundp 'c-no-indent-after-java-annotations)
+                             'c-no-indent-after-java-annotations)
+                          ,(when (fboundp 'c-lineup-assignments)
+                             'c-lineup-assignments)
+                          ++))
+                        (label . /)
+                        (case-label . +)
+                        (statement-case-open . +)
+                        (statement-case-intro . +) ; case w/o {
+                        (access-label . /)
+                        (innamespace . 0))))
+  "This C style mainly from Google C Style. Please refer to https://github.com/google/styleguide/blob/gh-pages/google-c-style.el")
 
 ;;;###autoload
-(defun athenacle|rcirc-load-credentials()
-  "This function read rcirc credentials in other file named private_things."
-  (require 'privacy_things)
-  (setq rcirc-server-alist athenacle|rcirc-server))
+(defun athenacle|setup-cc-mode()
+  (make-local-variable 'c-tab-always-indent)
+  (setq c-tab-always-indent t)
+  (c-add-style "Athenacle" athenacle|c-style t)
+  (c-toggle-auto-newline t)
+  (c-toggle-hungry-state t)
+  (setq c-default-style "Athenacle")
+  (setq tab-width 4)
+  (setq indent-tabs-mode nil))
 
 ;;;###autoload
-(defun athenacle|rcirc-hooks()
-  (when (configuration-layer/layer-usedp 'rcirc)
-    (add-hook 'rcirc-mode-hook 'athenacle|rcirc-load-credentials)))
+(defun athenacle|setup-c-modes-hooks()
+  (dolist (hook '(c-mode-hook c++-mode-hook objc-mode-hook))
+    (add-hook hook #'athenacle|setup-cc-mode)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; C mode style end.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;; rcirc end
-
-
-;;; elfeed start
-;;;###autoload
-(defun athenacle|elfeed-mark-all-as-read ()
-  "Mark all elfeed threads as read."
-  (interactive)
-  (mark-whole-buffer)
-  (elfeed-search-untag-all-unread))
 
 ;;;###autoload
-(defun athenacle|elfeed-hooks()
-  "This function will be called in dotspacemacs/user-config to initial key-bindings,"
-  (when (configuration-layer/layer-used-p 'elfeed)
-    (add-hook 'elfeed-new-entry-hook   (lambda() (define-key elfeed-search-mode-map (kbd "R") 'athenacle/elfeed-mark-all-as-read)))))
-
-(defun athenacle|add-hooks ()
-  (athenacle|rcirc-hooks)
-  (athenacle|elfeed-hooks))
-
+(defun athenacle|setup-others ()
+  "Setup other packages."
+  (athenacle|setup-c-modes-hooks))
 
 (provide 'init-others)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; init-others.el ends here
