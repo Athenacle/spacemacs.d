ca11bb50cd5b19fcc24dc7a3e655f89afa19b56c
WangXiao <zjjhwxc@gmail.com>
2018-02-16 01:34:27 +0800

    lisp/library: add macro athenacle||lib-macro

--- lisp/libraries.el
+++ lisp/libraries.el
@@ -89,20 +89,77 @@
 
 (defmacro athenacle||lib-call-if (pred func &optional object others)
   "Test function PRED, if it eval as not nil, apply OBJECT to FUNC. When FUNC is nil, set it to `message'."
-     `(when (funcall ,pred)
-       (apply (quote ,func) (cons ,object ,others))))
+  `(when (funcall ,pred)
+     (apply (quote ,func) (cons ,object ,others))))
 
 (defmacro athenacle||lib-call-if-debug-on-error (func &optional object others)
   "If `debug-on-error' is not nil, apply OBJECT to FUNC."
-    `(athenacle||lib-call-if
-     (lambda () (or init-file-debug debug-on-error))
-     ,func
-     ,object
-     ,others))
+  `(athenacle||lib-call-if
+    (lambda () (or init-file-debug debug-on-error))
+    ,func
+    ,object
+    ,others))
 
 (defmacro athenacle||lib-message-if-debug-on-error (object &optional others)
   "If `debug-on-error' is not nil, print OBJECT by `message'. If OTHERS is not nil, append it to paraments of `message'."
-     `(athenacle||lib-call-if-debug-on-error message ,object ,others))
+  `(athenacle||lib-call-if-debug-on-error message ,object ,others))
+
+
+
+(defun athenacle|lib-parse-args (keys args)
+  "Get KEYS arguments and body from ARGS."
+  (let ((ht (make-hash-table :test 'equal :size 5 :rehash-size 5))
+        (body '()))
+    (while args
+      (let* ((first (car args))
+             (next (cdr args)))
+        (if (and (symbolp first) (string-prefix-p ":" (symbol-name first)))
+            (let* ((name (symbol-name first))
+                   (short-name (substring name 1)))
+              (when (gethash short-name ht)
+                (error "Key %s already exist, previous value is %s" name (gethash short-name ht)))
+              (unless (member (intern short-name) keys)
+                (error "Key %s doesnot exist in keys parameter" name))
+              (unless next
+                (error "Need a keyword argument of %s" name))
+              (puthash short-name (car next) ht)
+              (setq next (cdr next)))
+          (add-to-list 'body first))
+        (setq args next)))
+    (cons ht (nreverse body))))
+
+
+(defun athenacle|lib-build-lambda-from-sexps (args list-of-sexp)
+  "Build lambda function from LIST-OF-SEXP, which signature is (lambda ARGS nil nil LIST-OF-SEXP)."
+  (unless (listp list-of-sexp)
+    (error "Error type %s of list-of-sexp" (type-of list-of-sexp)))
+  (unless (listp args)
+    (error "Error type %s of args" (type-of args)))
+  (let ((tmp-lambda list-of-sexp)
+        (func nil))
+    (add-to-list 'tmp-lambda nil)       ;; interactive
+    (add-to-list 'tmp-lambda nil)       ;; docstring
+    (add-to-list 'tmp-lambda args)      ;; args
+    (add-to-list 'tmp-lambda 'lambda)   ;; lambda
+    (eval tmp-lambda)))
+
+(cl-defmacro athenacle||lib-macro (name keys &rest rest)
+  "Build macro named NAME which has KEYS keyword parameter and REST is the body of function."
+  `(cl-defmacro ,name (&rest arg-list)
+     (let* ((ht-table (athenacle|lib-parse-args (quote ,keys) arg-list))
+            (ht (car ht-table))
+            (rest-body (cdr ht-table))
+            (actual-args '()))
+       (dolist (k (quote ,keys))
+         (push (gethash (format "%s" k) ht) actual-args))
+       (setq actual-args (nreverse actual-args))
+       (push (athenacle|lib-build-lambda-from-sexps nil rest-body) actual-args )
+         (apply
+          (lambda (body ,@keys)
+            (progn ,@rest))
+          actual-args))))
+
+(defalias 'ath-defmacro 'athenacle||lib-macro)
 
 
 (provide 'libraries)
